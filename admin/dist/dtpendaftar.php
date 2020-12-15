4c7970388478f5ff81d1ab4f997cd0f5b424a1df
nrnbl <58130204+nrnbl@users.noreply.github.com>
2020-12-15 20:15:43 +0700

    commit menu dan session

--- admin/dist/dtpendaftar.php
+++ admin/dist/dtpendaftar.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if ($_SESSION['kondisi'] != "login") {
+  header("location:../index.php?pesan=belum_login");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,47 +19,23 @@
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand" href="index.php">Start Bootstrap</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             
         </nav>
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.html">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                            <div class="sb-sidenav-menu-heading">Manajemen</div>
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Berita
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="kategori.html">Form Kategori</a>
-                                    <a class="nav-link" href="fberita.html">Form Berita</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Pendaftaran
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="dtpendaftar.html">Data Pendaftar</a>
-                                    <a class="nav-link" href="dtadmin.html">Data Admin</a>
-                                </nav>
-                            </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                <?php include "menu.php" ?>;
+                    <<div class="sb-sidenav-footer">
+                        <div class="small">Nama Admin :</div>
+                        <p class="text-gray-800">
+                        <!-- Page Heading -->
+                        <?php
+                        $username = $_SESSION['username'];
+                        echo $username;
+                        ?>
+                        </p>
                     </div>
                 </nav>
             </div>
@@ -62,7 +44,7 @@
                     <div class="container-fluid">
                         <h1 class="mt-4">Tables</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
                             <li class="breadcrumb-item active">Tables</li>
                         </ol>
                         <div class="card mb-4">
@@ -96,6 +78,7 @@
                             </div>
                         </div>
                     </div>
+                </div>
                 </main>
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
